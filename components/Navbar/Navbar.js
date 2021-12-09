dcbbc2dd5403cc3ffe69964ba7b94f3efc0a203b
sederamihaja <sederamihaja@gmail.com>
2021-12-09 16:14:23 +0300

    Edit navbar (align to right)

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -14,13 +14,13 @@ export default function Navbar() {
 
   return (
     <>
-      <nav class="navbar navbar-expand-md navbar-custom">
-        <div class="container-fluid">
-          <button class="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar">
-            <span class="navbar-toggler-icon"></span>
+      <nav className="navbar navbar-expand-md navbar-custom">
+        <div className="container-fluid">
+          <button className="navbar-toggler ms-auto" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNavbar">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
-            <ul class="navbar-nav ms-auto flex-nowrap">
+          <div className="collapse navbar-collapse flex-grow-1 text-right" id="myNavbar">
+            <ul className="navbar-nav ms-auto flex-nowrap">
               {NavLink.map((navLink, index) => {
                 return (
                   <li className="nav-item" key={index}>
@@ -46,51 +46,6 @@ export default function Navbar() {
           </div>
         </div>
       </nav>
-      {/* <nav className="navbar navbar-expand-md navbar-custom">
-        <span className="navbar-brand">
-          Test
-        </span>
-        
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav mr-auto">
-            <li className="nav-item">
-              a
-            </li>
-          </ul>
-          <ul className="navbar-nav">
-            {NavLink.map((navLink, index) => {
-              return (
-                <li className="nav-item" key={index}>
-                  <NextLink href={navLink.path}>
-                    <a 
-                      className="nav-link"
-                    >
-                      {navLink.label}
-                    </a>
-                  </NextLink>
-                </li>
-              );
-            })}
-            <li className="nav-item">
-              <span
-                className="nav-link"
-              >
-                <SvgCart />
-                {myCart?.length || 0}
-              </span>
-            </li>
-          </ul>
-        </div>
-      </nav> */}
     </>
   );
 }
